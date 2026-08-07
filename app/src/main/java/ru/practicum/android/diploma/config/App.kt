e2ac346bc4a0d70d73ead928a7aead4c3ad4521f
Diego <egoroof1@gmail.com>
2026-08-07 13:50:32 +0300

    create - config.App

---
+++ app/src/main/java/ru/practicum/android/diploma/config/App.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.config
+
+import android.app.Application
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Устанавливаем тему
+
+        // Инициализируем Koin
+    }
+}
