58aedb0f2a02ee2f2ade251493616b27710aa613
Diego <egoroof1@gmail.com>
2026-08-07 15:49:02 +0300

    fix: rules for code appearance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,9 @@ kotlin {
 
 dependencies {
     // compose
-    implementation ("androidx.compose.ui:ui:1.11.4")
+    implementation("androidx.compose.ui:ui:1.11.4")
     implementation("androidx.compose.material3:material3:1.4.0")
-    implementation ("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.11.4")
     implementation("androidx.compose.material:material:1.11.4")
     // для визуализации compose функций и замена Glide
