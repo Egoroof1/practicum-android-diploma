d2a1be766e3648ba9dc6a9ac1d9d34c656394b90
P@Cm4n <verigindanya@yandex.ru>
2026-08-09 19:41:18 +0300

    Изменил значения в Color

--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -2,18 +2,18 @@ package ru.practicum.android.diploma.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BlackDay = Color(0xFF1A1B22)
-val BlackNight = Color(0xFFFDFDFD)
+val BlackDay = Color(color = 0xFF1A1B22)
+val BlackNight = Color(color = 0xFFFDFDFD)
 
-val WhiteDay = Color(0xFFFDFDFD)
-val WhiteNight = Color(0xFF1A1B22)
+val WhiteDay = Color(color = 0xFFFDFDFD)
+val WhiteNight = Color(color = 0xFF1A1B22)
 
-val Blue = Color(0xFF3772E7)
-val Red = Color(0xFFF56B6C)
-val Gray = Color(0xFFAEAFB4)
-val LightGray = Color(0xFFFDFDFD)
+val Blue = Color(color = 0xFF3772E7)
+val Red = Color(color = 0xFFF56B6C)
+val Gray = Color(color = 0xFFAEAFB4)
+val LightGray = Color(color = 0xFFFDFDFD)
 
-val BlackUniversal = Color(0xFF1A1B22)
-val WhiteUniversal = Color(0xFFFDFDFD)
+val BlackUniversal = Color(color = 0xFF1A1B22)
+val WhiteUniversal = Color(color = 0xFFFDFDFD)
 
 val Overlay = BlackUniversal.copy(alpha = 0.5f)
