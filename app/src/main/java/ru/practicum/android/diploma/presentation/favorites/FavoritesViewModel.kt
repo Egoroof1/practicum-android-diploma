73e3a1e5249edac5fe9008a6851091c651f124e8
P@Cm4n <verigindanya@yandex.ru>
2026-08-17 20:09:02 +0300

    Реализован экран избранного

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
@@ -0,0 +1,32 @@
+package ru.practicum.android.diploma.presentation.favorites
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.db.VacancyInteractor
+
+class FavoritesViewModel(
+    private val vacancyInteractor: VacancyInteractor
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<FavoritesState>(FavoritesState.Loading)
+    val state: StateFlow<FavoritesState> = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            vacancyInteractor.getVacanciesList()
+                .catch { _state.value = FavoritesState.Error }
+                .collect { vacancies ->
+                    _state.value = if (vacancies.isEmpty()) {
+                        FavoritesState.Empty
+                    } else {
+                        FavoritesState.Content(vacancies)
+                    }
+                }
+        }
+    }
+}
