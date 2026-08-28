ef206ff9781e2fac44d8db17bd6dcf2d04ced18d
I_Am_Morald <slogoff@yandex.ru>
2026-08-28 21:59:47 +0300

    Убран диалог, чуть поработал над кодом.

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/FilterScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/FilterScreen.kt
@@ -5,16 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -36,18 +30,14 @@ fun FilterScreen(
     val industryName = stateFilter.selectedIndustry?.name
     val isFilterChanged = stateFilter.isFilterChanged
 
-    var showDiscardDialog by rememberSaveable { mutableStateOf(false) }
-
-    BackHandler(
+/*    BackHandler(
         enabled = true,
         onBack = {
-            if (stateFilter.isFilterChanged) {
-                showDiscardDialog = true
-            } else {
-                onBackClick()
-            }
+            viewModel.resetToLastAppliedFilter()
+            onBackClick()
+
         }
-    )
+    )*/
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -56,11 +46,8 @@ fun FilterScreen(
             AppTopBar(
                 title = stringResource(id = R.string.title_filter_settings),
                 onBackClick = {
-                    if (stateFilter.isFilterChanged) {
-                        showDiscardDialog = true
-                    } else {
-                        onBackClick()
-                    }
+                    viewModel.resetToLastAppliedFilter()
+                    onBackClick()
                 },
             )
         },
@@ -88,55 +75,13 @@ fun FilterScreen(
             Spacer(modifier = Modifier.weight(1f))
             if (isFilterChanged) {
                 FilterButtons(
-                    onApplyClick = onApplyClick,
-                    onResetClick = {
-                        viewModel.clearIndustry()
-                        viewModel.onSalaryChange("")
-                        viewModel.isOnlySalaryChanged(false)
-                        viewModel.isFilterCleared()
+                    onApplyClick = {
+                        viewModel.setFilter()
+                        onApplyClick()
                     },
+                    onResetClick = { viewModel.clearFilterState() },
                 )
             }
         }
-    }
-
-    if (showDiscardDialog) {
-        AlertDialog(
-            onDismissRequest = { showDiscardDialog = false },
-            title = {
-                Text(
-                    text = "Сохранить изменения?",
-                    style = MaterialTheme.typography.titleMedium
-                )
-            },
-            text = {
-                Text(
-                    text = "Настройки фильтрации изменились.",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        showDiscardDialog = false
-                        viewModel.setFilter()
-                        onBackClick()
-                    }
-                ) {
-                    Text("Да")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        showDiscardDialog = false
-                        viewModel.resetToLastAppliedFilter()
-                        onBackClick()
-                    }
-                ) {
-                    Text("Нет")
-                }
-            }
-        )
     }
 }
